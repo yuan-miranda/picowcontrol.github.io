c35c8d3e583b829c154e8f68618a9f2ed4ffcf6f
yuan-miranda <yuanezekielamiranda@gmail.com>
2024-12-24 17:51:47 +0800

    wierd thing testing for mobile

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,8 @@ import { loadHeader } from "./HEADER.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
     await loadHeader();
+    
+    // prompt the user screen size
+    const screenSize = window.innerWidth;
+    alert(screenSize);
 });
