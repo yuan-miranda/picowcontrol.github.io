1b4ebe1a742cae9fafa6998590bc51e807d38de4
yuan-miranda <yuanezekielamiranda@gmail.com>
2024-12-31 00:04:32 +0800

    added control functionality and corrected a pin

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ led = machine.Pin("LED", machine.Pin.OUT)
 
 # control pins
 forward = machine.Pin(14, machine.Pin.OUT)
-backward = machine.Pin(15, machine.Pin.OUT)
+backward = machine.Pin(17, machine.Pin.OUT)
 right = machine.Pin(4, machine.Pin.OUT)
 left = machine.Pin(2, machine.Pin.OUT)
 gear_speed = machine.Pin(28, machine.Pin.OUT)
@@ -162,10 +162,6 @@ async def handle_request(request):
         # a list of actions from "scripts" directory which in turn, "actions" are the .py files inside
         # that folder. The "scripts" directory is located in the root directory of this project/pico.
 
-        # TEMP: didnt think about how i would actually execute those scripts, but my idea was to kind of
-        # "include" the codes in there to this main.py file, sort of like a "extension" to organize and
-        # separate each action into their own thing.
-
         # sends a list of scripts in the "scripts" directory
         elif request_path == "/api/get/scripts":
             scripts = os.listdir("scripts")
@@ -192,6 +188,38 @@ async def handle_request(request):
             json_content = json.dumps(response)
             await send_response(conn, "application/json", json_content.encode())
             return
+
+        # ill just place the actions here for now as I was still testing the functionality
+        # /api/run/action/control/000000
+        elif "/api/run/action/control/" in request_path:
+            try:
+                # take the string after /api/run/action/control/
+                control_states = request_path.split("/api/run/action/control/")[-1]
+                if len(control_states) != 6 or not control_states.isdigit():
+                    raise Exception("invalid control states")
+
+                # flags
+                is_forward = control_states[0] == "1"
+                is_backward = control_states[1] == "1"
+                is_gear_speed = control_states[2] == "1"
+                is_toggle_mode = control_states[3] == "1"
+                is_right = control_states[4] == "1"
+                is_left = control_states[5] == "1"
+
+                # set values
+                forward.value(is_forward)
+                backward.value(is_backward)
+                gear_speed.value(is_gear_speed)
+                toggle_mode.value(is_toggle_mode)
+                right.value(is_right)
+                left.value(is_left)
+
+                await send_response(conn, "text/plain", "200 OK".encode())
+                return
+            except Exception as e:
+                log(func__name__, 3, f"exception: {e}")
+                await send_response(conn, "text/plain", "500 Internal Server Error".encode())
+                return
 
         else:
             path = f"static{request_path}"
